ae70ad45ed6b6dfd1f78be3ff2e16df6f3f0cacb
superiorcamel <willrevell@hotmail.com>
2018-05-05 16:47:42 -0600

    fixed squirrel ai

--- Project Falcon/Assets/rabbit_AI.cs
+++ Project Falcon/Assets/rabbit_AI.cs
@@ -22,6 +22,13 @@ public class rabbit_AI : MonoBehaviour {
         {
 
             transform.position = Vector2.MoveTowards(transform.position, Anchor.transform.position, speed * Time.deltaTime);
+            Vector3 diff = Anchor.transform.position - gameObject.transform.position;
+            
+            diff.Normalize();
+
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 270);
+        }
 
 
 
@@ -31,6 +38,14 @@ public class rabbit_AI : MonoBehaviour {
                 gameObject.SendMessage("Kill");
             }
 
+        }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "anchor")
+        {
+            coll.gameObject.SendMessage("Damage");
+            Destroy(gameObject);
         }
     }
 }
