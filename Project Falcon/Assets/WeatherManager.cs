c551b40122fea8568b8005bc1d44e5d7ccde5ed0
mkp003 <mkp003@mail.usask.ca>
2018-05-05 13:44:19 -0600

    added rain functionality and tornado spawning.

--- Project Falcon/Assets/WeatherManager.cs
+++ Project Falcon/Assets/WeatherManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class WeatherManager : MonoBehaviour {
 
+    // Reference to partical system
+    private ParticleSystem particalSystem;
+
+    // Reference to the Tornado prefab
+    [SerializeField]
+    private GameObject tornadoPrefab;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+		this.particalSystem = GetComponent<ParticleSystem>();
+        this.SpawnTornado();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,10 +25,58 @@ public class WeatherManager : MonoBehaviour {
 
 
     /// <summary>
+    /// Start rainning
+    /// </summary>
+    void StartRain()
+    {
+        var emission = this.particalSystem.emission;
+        emission.enabled = true;
+    }
+
+
+    /// <summary>
+    /// Stop rainning
+    /// </summary>
+    void StopRain()
+    {
+        var emission = this.particalSystem.emission;
+        emission.enabled = false;
+    }
+
+
+    /// <summary>
     /// SpawnTornado() will create a tornado
     /// </summary>
     void SpawnTornado()
     {
+        // Determine the starting wall position where 1 = top and go clockwise
+        int wall = Random.Range(0, 4);
 
+        int positionX = 0;
+        int positionY = 0;
+
+        if (wall == 1)
+        {
+            positionX = Random.Range(-101, 100);
+            positionY = 100;
+        }
+        else if (wall == 2)
+        {
+            positionY = Random.Range(-101, 100);
+            positionX = 100;
+        }
+        else if (wall == 3)
+        {
+            positionX = Random.Range(-101, 100);
+            positionY = -100;
+        }
+        else
+        {
+            positionY = Random.Range(-101, 100);
+            positionX = -100;
+        }
+
+        // Instantiate tornado
+        GameObject newTornado = Instantiate(this.tornadoPrefab, new Vector3(positionX, positionY, 0));
     }
 }
