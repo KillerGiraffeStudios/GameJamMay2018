418cf1fae123c166739946d99bee88399d6ed892
Alexander Russell <ajex94@gmail.com>
2018-05-05 11:48:08 -0600

    Movement Tests

--- Project Falcon/Assets/Movement.cs
+++ Project Falcon/Assets/Movement.cs
@@ -6,9 +6,9 @@ using GamepadInput;
 public class Movement : MonoBehaviour {
 	public GamePad.Index Player = GamePad.Index.One;
 	private Rigidbody2D r_body;
-	public float acceleration = 0;
-	public float turnSpeed = 1;
-	public float maxVelocity;
+	// public float acceleration = 0;
+	// public float turnSpeed = 1;
+	// public float maxVelocity;
 	// Use this for initialization
 	void Start () {
 		r_body = GetComponent<Rigidbody2D>();
@@ -19,16 +19,21 @@ public class Movement : MonoBehaviour {
 	void Update () {
 		leftStick = GamePad.GetAxis(GamePad.Axis.LeftStick, Player);
 		// leftStick.Normalize();
-		transform.Rotate(0,0,-leftStick.x * turnSpeed);
+		transform.Rotate(0,0,-leftStick.x * MetaScript.getTurnSpeed());
 		
 		float rotation = Mathf.Deg2Rad * (transform.eulerAngles.z);
-		r_body.AddForce(new Vector2(Mathf.Cos(rotation),Mathf.Sin(rotation)) * acceleration);
 
+		if(GamePad.GetButton(GamePad.Button.B, Player)){
+			r_body.AddForce(r_body.velocity * -4);
+		}else if(GamePad.GetButton(GamePad.Button.A,Player)){
+			r_body.AddForce(new Vector2(Mathf.Cos(rotation),Mathf.Sin(rotation)) * MetaScript.getAcceleration());
+		}
+		
 
 		// Vector2 dir = leftStick * r_body.velocity;
 
-		if(r_body.velocity.sqrMagnitude > maxVelocity * maxVelocity){
-			r_body.velocity = r_body.velocity.normalized * maxVelocity;
+		if(r_body.velocity.sqrMagnitude > Mathf.Pow(MetaScript.getMaxSpeed(),2)){
+			r_body.velocity = r_body.velocity.normalized * MetaScript.getMaxSpeed();
 		}
 		// if(dir.x<0){
 		// 	leftStick.y *= 2;
