cc947084c9f8a816af243234efff980ffbb437bc
ConnorNG <netco_nessa@hotmail.com>
2018-05-06 14:28:03 -0600

    Added heal pickup

--- Project Falcon/Assets/Scripts/SpawnTreeHealth.cs
+++ Project Falcon/Assets/Scripts/SpawnTreeHealth.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class SpawnTreeHealth : Health {
 
     public GameObject explosion;
+    public GameObject heal; 
 
     public override void Kill() {
         this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
         this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
         this.gameObject.GetComponent<TreeSpawner>().SetDead();
         GlobalValues.oakTreesKilled += 1;
+        if(Random.Range(1,5) <= 2) {
+            Instantiate(heal, this.transform.position, this.transform.rotation);
+        }
+        
     }
     void OnTriggerEnter2D(Collider2D other) {
         if(other.name == "bullet(Clone)") {
---
+++ Project Falcon/Assets/healPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healPickup : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "anchor") {
+            Health anchorHP = other.gameObject.gameObject.GetComponent<Health>();
+            anchorHP.currentHealth = anchorHP.currentHealth + anchorHP.maxHealth;
+            destroy(gameObject);
+        }
+    }
+}
