c198f3ffa0b1eefb49822eb2d34c65fe817ee300
mkp003 <mkp003@mail.usask.ca>
2018-05-05 20:25:08 -0600

    Added camera script that follows the player.

---
+++ Project Falcon/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    // Reference to the players
+    [SerializeField]
+    private GameObject anchor;
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(anchor.position.transform.x, anchor.position.transform.y, 0);
+	}
+}
